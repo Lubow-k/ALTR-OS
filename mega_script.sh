d0237f521be111075d4d422730a32c287205de02
Smart-Peach <115448517+Smart-Peach@users.noreply.github.com>
2023-09-22 11:09:27 +0700

    New script

--- mega_script.sh
+++ mega_script.sh
@@ -2,7 +2,6 @@
 nasm -fbin boot.asm -o boot.bin
 dd if=/dev/zero of=boot.img bs=1024 count=1440
 dd if=boot.bin of=boot.img conv=notrunc
-export PATH=$PATH:"cygdrive/c/Program Files/Bochs-2.7"
 bochs -f os.bxrc -q
 rm boot.img
 rm boot.bin
