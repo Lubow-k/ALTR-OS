8a8e7d1894c5296a3e1d88b18f0f3f5342b7e3d2
Lubow-k <luba1607@gmail.com>
2023-09-22 18:29:12 +0100

    Build script with optional debug parameter

---
+++ linkage.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+nasm -fbin $1 -o out.bin  #$1 arg - name_file.asm
+
+dd if=/dev/zero of=out.img bs=1024 count=1440
+
+dd if=out.bin of=out.img conv=notrunc
+
+
+if [ $2 = "-d" ]   #$2 arg - optional -d (for debug)
+then
+    bochsdbg -f os-dbg.bxrc -q
+else
+    bochs -f os.bxrc -q
+fi
+
+rm out.bin
+rm out.img
