6c115f6100bce480f8339049b94f6900ec8b682f
Luba Kolbasova <luba1607@gmail.com>
2023-10-02 21:07:31 +0700

    add test.py for check_sum

--- test.py
+++ test.py
@@ -1,8 +1,23 @@
 import os
 
-size = 393216
+'''
+size = 392704   #393216 is 384K
 f = open("test.bin", "wb")
 line = os.urandom(size)
 f.write(line)
 count_sum = sum(line)
-print(count_sum)
+'''
+
+count_sum = 0
+
+with open("out.bin", "rb") as file:
+    number = list(file.read())
+    count_sum += sum(number)
+
+with open("test.bin", "rb") as file:
+    number = list(file.read())
+    count_sum += sum(number)
+
+print(hex(count_sum))
+
+
