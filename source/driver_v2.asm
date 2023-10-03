4eb4989a3b5cc53fe738ae396d406ceccb010f79
Smart <the_smartest_peach@mail.ru>
2023-10-03 14:28:58 +0100

    error processing

--- source/driver_v2.asm
+++ source/driver_v2.asm
@@ -11,6 +11,8 @@ mov ds, ax        ; Correct data segment
 mov ax, 0x2000    ; Correct extended segment
 mov es, ax
 
+mov ah, 2         ; Read sectors from drive
+mov al, 1         ; Number of sectors to read
 mov ch, 0         ; Cylinder number
 mov cl, 1         ; Starting sector number
 xor dh, dh        ; Set head
@@ -24,25 +26,30 @@ main:
         xor bx, bx           ; Clear bx
 
     read_int:
-        mov ah, 2            ; Read sectors from drive
-        mov al, 1            ; Number of sectors to read
+        mov ah, 2         ; Read sectors from drive
+        mov al, 1         ; Number of sectors to read
         int 0x13
-        jc read_int          ; Test if read sucsess
-        
+        jc error_message      ; Test if read successfully
+
+
 ; TODO check for errors
 
         mov bx, es
-        add bx, 0x20              ; Add to bx 512 (1 sectors)
-        mov es, bx                ; Move es
-        xor bx, bx                ; Clear bx
+        add bx, 0x20          ; Add to bx 512 (1 sectors)
+        mov es, bx            ; Move es
+        xor bx, bx            ; Clear bx
 
-        inc cl                    ; Increment sector number
-        cmp cl, 0x13              ; Check if we on 19th sector
-        jz check_increment_head   ; Then need to increment more
-        jmp check_if_end          ; Continue reading
+        inc cl                ; Increment sector number
+
+        test bx, bx           ; Clear flags
+        cmp cl, 0x13          ; Check if we on 19th sector
+        jz check_increment_head; Then need to increment more
+        jmp check_if_end      ; Continue reading
 
     check_increment_head:
         mov cl, 0x1           ; Set sectors to 1
+
+        test bx, bx           ; Clear flags
         cmp dh, 0             ; Check if head is 0
         jz increment_head     ; If zero increment
         jmp decrement_head    ; If one decrement
@@ -57,11 +64,22 @@ main:
 
     check_if_end:
         pop bx                ; Get counter
+
+        test bx, bx           ; Clear flags
         cmp bx, 0x300         ; Check if we read 768 sectors
-        jz count_sum          ; Exit loop
+        jz end                ; Exit loop
         jmp read_loop         ; Repeat
 
-count_sum:
+
+error_message:
+    inc bx                     ; bx -> aka counter for 3 loop
+    cmp bx, 0x4                ; if we read disk more than 3 times -> problems with disk
+    jz end                     ; If problems with disk --> start counting sum to see error
+    jmp read_int               ; Try to read from disk one more time
+
+
+
+end:
     mov ax, 0x2000
     mov es, ax
 
@@ -69,7 +87,7 @@ count_sum:
     xor bx, bx                ; Initiate counter under 16
 
     sum_loop:
-        add al, byte [es:bx]  
+        add al, byte [es:bx]  ; May be error
         inc bx
         cmp bx, 0x10          ; Check if 16
         jz move_es            ; increment es
@@ -77,13 +95,13 @@ count_sum:
         move_es:
             mov bx, es        ; Get es
             inc bx            ; Increment es
-            cmp bx, 0x8000    ; Check if thats all
-            jz end
+            cmp bx, 0x8000    ; Check if that's all
+            jz end_end
             mov es, bx
             xor bx, bx        ; Get null to counter
             jmp sum_loop      ; Repeat
 
-end:     
+end_end:
     loop:
         jmp loop
 
