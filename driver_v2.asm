0a43197bceac18ecd7670cf2edf66b5f6d0174d7
Luba Kolbasova <luba1607@gmail.com>
2023-10-03 15:45:42 +0700

    Correct driver

--- driver_v2.asm
+++ driver_v2.asm
@@ -12,7 +12,7 @@ mov ax, 0x2000    ; Correct extended segment
 mov es, ax
 
 mov ah, 2         ; Read sectors from drive
-mov al, 2         ; Number of sectors to read
+mov al, 1         ; Number of sectors to read
 mov ch, 0         ; Cylinder number
 mov cl, 1         ; Starting sector number
 xor dh, dh        ; Set head
@@ -21,14 +21,14 @@ xor bx, bx        ; Clear bx register
 main:
     read_loop:
 ; Move after check errors
-        add bx, 0x1          ; Increment counter
+        inc bx               ; Increment counter
         push bx              ; Push counter on stack
         xor bx, bx           ; Clear bx
 
     read_int:
-        test bx, bx
+        mov ah, 2         ; Read sectors from drive
+        mov al, 1         ; Number of sectors to read
         int 0x13
-
         jc read_int          ; Test if read sucsess
         
 ; TODO check for errors
@@ -38,7 +38,7 @@ main:
         mov es, bx            ; Move es
         xor bx, bx            ; Clear bx
 
-        add cl, 0x1           ; Increment sector number
+        inc cl                ; Increment sector number
 
         test bx, bx           ; Clear flags
         cmp cl, 0x13          ; Check if we on 19th sector
@@ -69,28 +69,31 @@ main:
         jz end                ; Exit loop
         jmp read_loop         ; Repeat
 
-; end:
-;     mov ax, 0x2000
-;     mov es, ax
-
-;     xor ax, ax                ; Initiate sum
-;     xor bx, bx                ; Initiate counter under 16
-
-;     sum_loop:
-;         add al, byte [es:bx]  ; May be error
-;         inc bx
-;         cmp bx, 0x10          ; Check if 16
-;         jz move_es            ; increment es
-;         jmp sum_loop          ; Repeat
-;         move_es:
-;             mov bx, es        ; Get es
-;             inc bx            ; Increment es
-;             cmp bx, 0x8000    ; Check if thats all
-;             jz end_end
-;             mov es, bx
-;             xor bx, bx        ; Get null to counter
-;             jmp sum_loop      ; Repeat
 end:
+    mov ax, 0x2000
+    mov es, ax
+
+    xor ax, ax                ; Initiate sum
+    xor bx, bx                ; Initiate counter under 16
+
+    sum_loop:
+        add al, byte [es:bx]  ; May be error
+        inc bx
+        cmp bx, 0x10          ; Check if 16
+        jz move_es            ; increment es
+        jmp sum_loop          ; Repeat
+        move_es:
+            mov bx, es        ; Get es
+            inc bx            ; Increment es
+            cmp bx, 0x8000    ; Check if thats all
+            jz end_end
+            mov es, bx
+            xor bx, bx        ; Get null to counter
+            jmp sum_loop      ; Repeat
+
 end_end:     
+    loop:
+        jmp loop
+
     times 510-($-$$) db 0
     dw 0xAA55
