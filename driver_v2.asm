ca016bc5982ca21bc8b7cded735dbd131dcc688f
Luba Kolbasova <luba1607@gmail.com>
2023-10-03 10:59:13 +0700

    Some errors

---
+++ driver_v2.asm
@@ -0,0 +1,86 @@
+[BITS 16]
+
+cli
+xor ax, ax        ; |
+mov ss, ax        ; |
+mov sp, 0x7C00    ; |Correct stack segment
+
+mov ax, 0x7C0
+mov ds, ax        ; Correct data segment
+
+mov ax, 0x2000    ; Correct extended segment
+mov es, ax
+
+mov ah, 2         ; Read sectors from drive
+mov al, 2         ; Number of sectors to read
+mov ch, 0         ; Cylinder number
+mov cl, 1         ; Starting sector number
+xor dh, dh        ; Set head
+xor bx, bx        ; Clear bx register
+
+main:
+    read_loop:
+; Move after check errors
+        add bx, 0x2          ; Increment counter
+        push bx              ; Push counter on stack
+        xor bx, bx           ; Clear bx
+
+        int 0x13
+; TODO check for errors
+
+        mov bx, es
+        add bx, 0x40          ; Add to bx 1024 (2 sectors)
+        mov es, bx            ; Move es
+
+        add cl, 0x2           ; Increment sector number
+
+        cmp cl, 0x13          ; Check if we on 19th sector
+        jz check_increment_head; Then need to increment more
+        jmp check_if_end      ; Continue reading
+
+    check_increment_head:
+        mov cl, 0x1           ; Set sectors to 1
+
+        cmp dh, 0             ; Check if head is 0
+        jz increment_head     ; If zero increment
+        jmp decrement_head    ; If one decrement
+
+    increment_head:
+            inc dh            ; Increment head number
+            jmp check_if_end
+
+    decrement_head:
+            dec dh            ; Decrement head number
+            inc ch            ; then increment cylinder number
+
+    check_if_end:
+        pop bx                ; Get counter
+        cmp bx, 0x300         ; Check if we read 768 sectors
+        jz end                ; Exit loop
+        jmp read_loop         ; Repeat
+
+end:
+    mov ax, 0x2000
+    mov es, ax
+
+    xor ax, ax                ; Initiate sum
+    xor bx, bx                ; Initiate counter under 16
+
+    sum_loop:
+        add al, byte [es:bx]  ; May be error
+        inc bx
+        cmp bx, 0x10          ; Check if 16
+        jz move_es            ; increment es
+        jmp sum_loop          ; Repeat
+        move_es:
+            mov bx, es        ; Get es
+            inc bx            ; Increment es
+            cmp bx, 0x8000    ; Check if thats all
+            jz end_end
+            mov es, bx
+            xor bx, bx        ; Get null to counter
+            jmp sum_loop      ; Repeat
+
+end_end:     
+    times 510-($-$$) db 0
+    dw 0xAA55
--- link.sh
+++ link.sh
@@ -4,9 +4,7 @@ nasm -fbin $1 -o out.bin  #$1 arg - name_file.asm
 
 dd if=/dev/zero of=out.img bs=1024 count=1440
 dd if=out.bin of=out.img conv=notrunc
-
-dd if=test.bin of=boot.img conv=notrunc seek=1  #kernel.bin after
-
+dd if=test.bin of=out.img conv=notrunc seek=1
 
 if [ $2 = "-d" ]   #$2 arg - optional -d (for debug)
 then
@@ -15,5 +13,5 @@ else
     bochs -f os.bxrc -q
 fi
 
-rm out.bin
-rm out.img
+#out.bin
+#rm out.img
