70f66516007c68ee61c070b7449f27541ee78a6a
Smart <the_smartest_peach@mail.ru>
2023-10-05 15:32:30 +0100

    new dh inc + free bx + sorry Luba

--- source/driver_v2.asm
+++ source/driver_v2.asm
@@ -1,86 +1,81 @@
 [BITS 16]
 
 cli
-xor ax, ax        ; |
-mov ss, ax        ; |
-mov sp, 0x7C00    ; |Correct stack segment
+xor ax, ax                       ; |
+mov ss, ax                       ; |
+mov sp, 0x7C00                   ; |Correct stack segment
 
 mov ax, 0x7C0
-mov ds, ax        ; Correct data segment
+mov ds, ax                       ; Correct data segment
 
-mov ax, 0x2000    ; Correct extended segment
+mov ax, 0x2000                   ; Correct extended segment
 mov es, ax
 
-mov ch, 0         ; Cylinder number
-mov cl, 1         ; Starting sector number
-xor dh, dh        ; Set head 0
-xor bx, bx        ; Clear bx register
-xor di, di        ; Clear global counter
+mov ch, 0                        ; Cylinder number
+mov cl, 1                        ; Starting sector number
+xor dh, dh                       ; Set head 0
+xor bx, bx                       ; Clear bx register --> Always 0 for int
+mov di, 0x2000                   ; Clear global counter
 
 main:
     read_loop:
-        xor si, si              ; Clear counter for error loop
-        inc di                  ; Increment counter
-        xor bx, bx              ; Clear bx
+        xor si, si               ; Clear counter for error loop
 
     read_int:
-        mov ah, 2               ; Read sectors from drive
-        mov al, 1               ; Number of sectors to read
+        mov ah, 2                ; Read sectors from drive
+        mov al, 1                ; Number of sectors to read
         int 0x13
-        jc error_message        ; Test if read successfully
+        jc error_message         ; Test if read successfully
 
-        mov bx, es
-        add bx, 0x20            ; Add to bx 512 (1 sectors)
-        mov es, bx              ; Move es
-        xor bx, bx              ; Clear bx
+        add di, 0x20             ; Add 200 (move 200 after reading one sector)
+        mov es, di               ; Move es
 
-        inc cl                  ; Increment sector number
-        cmp cl, 0x13            ; Check if we on 19th sector
-        jnz check_if_end        ; Then need to increment more
+        inc cl                   ; Increment sector number
+        cmp cl, 0x13             ; Check if we on 19th sector
+        jnz check_if_end         ; Then need to increment more
 
-        mov cl, 0x1             ; Set sectors to 1
-        inc dh                  ; | Increment head
-        and dh, 0x1             ; |
-        cmp dh, 0x0             ; If head became 0 --> we done 1 cylinder
-        jnz check_if_end        ; If not check if we finished
-        inc ch                  ; Increment cylinder number
+        mov cl, 0x1              ; Set sectors to 1
+        inc dh                   ; | Increment head
+        cmp dh, 0x2              ; If head became 2 --> we done 1 cylinder + reset head
+        jnz check_if_end         ; If not check if we finished
+        xor dh, dh
+        inc ch                   ; Increment cylinder number
 
     check_if_end:
-        cmp di, 0x300           ; Check if we read 768 sectors
-        jz count_sum            ; Exit loop
-        jmp read_loop           ; Repeat
+        cmp di, 0x8000           ; Check if we read 768 sectors -->0x0
+        jz count_sum             ; Exit loop
+        jmp read_loop            ; Repeat
 
 error_message:
-    inc si                      ; Increment the bad tries to read from disk
-    cmp si, 0x4                 ; | If we tried more than 3 times -> finish
-    jz end                      ; |
-    jmp read_int                ; Try to read from disk one more time
+    inc si                       ; Increment the bad tries to read from disk
+    cmp si, 0x4                  ; | If we tried more than 3 times -> finish
+    jz end                       ; |
+    jmp read_int                 ; Try to read from disk one more time
 
 count_sum:
     mov ax, 0x2000
     mov es, ax
 
-    xor ax, ax                  ; Initiate sum
-    xor bx, bx                  ; Initiate counter under 16
+    xor ax, ax                   ; Initiate sum
+    xor bx, bx                   ; Initiate counter under 16
 
     sum_loop:
         add al, byte [es:bx]
         inc bx
-        cmp bx, 0x10            ; Check if 16
-        jz move_es              ; increment es
-        jmp sum_loop            ; Repeat
+        cmp bx, 0x10             ; Check if 16
+        jz move_es               ; increment es
+        jmp sum_loop             ; Repeat
         move_es:
-            mov bx, es          ; Get es
-            inc bx              ; Increment es
-            cmp bx, 0x8000      ; Check if that's all
+            mov bx, es           ; Get es
+            inc bx               ; Increment es
+            cmp bx, 0x8000       ; Check if that's all
             jz end
             mov es, bx
-            xor bx, bx          ; Get null to counter
-            jmp sum_loop        ; Repeat
+            xor bx, bx           ; Get null to counter
+            jmp sum_loop         ; Repeat
 
 end:
-    loop:
-        jmp loop
+    jmp end
 
-    times 510-($-$$) db 0
-    dw 0xAA55
+times 510-($-$$) db 0
+dw 0xAA55
