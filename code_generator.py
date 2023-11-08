f97d7ad34b807c1eb4bd1312711e44d087f36120
Luba Kolbasova <luba1607@gmail.com>
2023-11-08 18:14:00 +0700

    add code_generator

--- code_generator.py
+++ code_generator.py
@@ -1,7 +1,7 @@
-# for i in range(256):
-#     print("static void tramplin_"+ str(hex(i))[2:].upper() + "() { panic_handler(" + str(hex(i)).upper() + "); }")
-s = "static void* tramplins = {"
 for i in range(256):
-    s += "tramplin_" + str(hex(i))[2:].upper() + ", "
+    print("static void tramplin_"+ str(hex(i))[2:].upper() + "();")
+s = "static void* tramplins = {"
+# for i in range(256):
+#     s += "tramplin_" + str(hex(i))[2:].upper() + ", "
 
-print(s)
+# print(s)
