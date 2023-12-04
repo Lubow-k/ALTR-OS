d65adb89fab2fb8f0e4e0788313e4f1b9582dd83
malaartyom <malarenkoart@bk.ru>
2023-12-04 17:22:54 +0700

    create 7

--- code_generator.py
+++ code_generator.py
@@ -1,7 +1,51 @@
-for i in range(256):
-    print(f"tramplins[{i}] = tramplin_{hex(i).upper()[2:]};")
+# for i in range(256):
+#     print(f"tramplins[{i}] = tramplin_{hex(i).upper()[2:]};")
 # s = "static void* tramplins = {"
 # for i in range(256):
 #     s += "tramplin_" + str(hex(i))[2:].upper() + ", "
 
 # print(s)
+
+D = {"0" : "0",
+     "1" : "0",
+     "2" : "0",
+     "3" : "0",
+     "4" : "0",
+     "5" : "0",
+     "6" : "0",
+     "7" : "0",
+     "8" : "1",
+     "9" : "0",
+     "10" : "1",
+     "11" : "1",
+     "12" : "1",
+     "13" : "1",
+     "14" : "1",
+     "15" : "0",
+     "16" : "0",
+     "17" : "1",
+     "18" : "0",
+     "19" : "0",
+     "20" : "0",
+     "21" : "1",
+     "22" : "0",
+     "23" : "0",
+     "24" : "0",
+     "25" : "0",
+     "26" : "0",
+     "27" : "0",
+     "28" : "0",
+     "29" : "1",
+     "30" : "1",
+     "31" : "0",
+     }
+
+for key, val in D.items():
+    print()
+    if val == "0":
+        print("tramplin_" + str(hex(int(key))).upper())
+        print("    " + "push 0xDEAD")
+        print("    " + "push " + str(hex(int(key))).upper())
+    else:
+        print("tramplin_" + str(hex(int(key))).upper())
+        print("    " + "push " + str(hex(int(key))).upper())
---
+++ source/allocator/seven.asm
@@ -0,0 +1,125 @@
+[BITS 32]
+tramplin_0X0
+    push 0xDEAD
+    push 0X0
+
+tramplin_0X1
+    push 0xDEAD
+    push 0X1
+
+tramplin_0X2
+    push 0xDEAD
+    push 0X2
+
+tramplin_0X3
+    push 0xDEAD
+    push 0X3
+
+tramplin_0X4
+    push 0xDEAD
+    push 0X4
+
+tramplin_0X5
+    push 0xDEAD
+    push 0X5
+
+tramplin_0X6
+    push 0xDEAD
+    push 0X6
+
+tramplin_0X7
+    push 0xDEAD
+    push 0X7
+
+tramplin_0X8
+    push 0X8
+
+tramplin_0X9
+    push 0xDEAD
+    push 0X9
+
+tramplin_0XA
+    push 0XA
+
+tramplin_0XB
+    push 0XB
+
+tramplin_0XC
+    push 0XC
+
+tramplin_0XD
+    push 0XD
+
+tramplin_0XE
+    push 0XE
+
+tramplin_0XF
+    push 0xDEAD
+    push 0XF
+
+tramplin_0X10
+    push 0xDEAD
+    push 0X10
+
+tramplin_0X11
+    push 0X11
+
+tramplin_0X12
+    push 0xDEAD
+    push 0X12
+
+tramplin_0X13
+    push 0xDEAD
+    push 0X13
+
+tramplin_0X14
+    push 0xDEAD
+    push 0X14
+
+tramplin_0X15
+    push 0X15
+
+tramplin_0X16
+    push 0xDEAD
+    push 0X16
+
+tramplin_0X17
+    push 0xDEAD
+    push 0X17
+
+tramplin_0X18
+    push 0xDEAD
+    push 0X18
+
+tramplin_0X19
+    push 0xDEAD
+    push 0X19
+
+tramplin_0X1A
+    push 0xDEAD
+    push 0X1A
+
+tramplin_0X1B
+    push 0xDEAD
+    push 0X1B
+
+tramplin_0X1C
+    push 0xDEAD
+    push 0X1C
+
+tramplin_0X1D
+    push 0X1D
+
+tramplin_0X1E
+    push 0X1E
+
+tramplin_0X1F
+    push 0xDEAD
+    push 0X1F
+
+collect_context:
+    push ds
+    push es
+    push fs
+    push gs
+    pusha
